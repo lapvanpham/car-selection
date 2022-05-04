dbfc945757d6a6d598ed51bf8066061f2ebe1bd9
Lap Van Pham <caramel-apple@outlook.com>
2022-05-04 18:59:09 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
+  const carList = [
+    {
+      model: 'Cadillac CT5',
+      color: ['Red', 'Blue', 'Green'],
+    },
+    {
+      model: 'BMW X5',
+      color: ['Salmon', 'Pink', 'Light Coral'],
+    },
+    {
+      model: 'Audi A8',
+      color: ['Hot Pink', 'Black', 'Crimson'],
+    },
+    {
+      model: 'Mercedes-Benz SLS',
+      color: ['Papaya Whip', 'Orchid', 'Olive'],
+    },
+  ];
+
+  const [selectedCar, setSelectedCar] = useState(carList[0]);
+
+  const handleModelChange = function (event) {
+    const selectedModel = carList.find(
+      (car) => car.model === event.target.value
+    );
+    setSelectedCar(selectedModel);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Car List</h1>
+      <div className='select-group'>
+        <label for='cars'>Choose a car:</label>
+
+        <select name='cars' id='cars' onChange={handleModelChange}>
+          {carList.map((car) => {
+            return <option value={car.model}>{car.model}</option>;
+          })}
+        </select>
+      </div>
+
+      <div className='select-group'>
+        <label for='colors'>Choose a color:</label>
+        <select name='colors' id='colors'>
+          {selectedCar.color.map((color) => {
+            return <option value={color}>{color}</option>;
+          })}
+        </select>
+      </div>
     </div>
   );
 }
